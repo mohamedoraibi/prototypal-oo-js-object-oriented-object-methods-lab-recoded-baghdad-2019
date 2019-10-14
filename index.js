00eb3c4ea3b14a16432d1a5318ed939ed4e27e65
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-14 09:51:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,24 @@
+class BoardMember{
+  constructor(name){
+    this.name=name;
+    this.homeState=homeState;
+    this.training=training;
+  }
+  veto(){
+    returns `No, I must disagree`;
+  }
+}
+
+
+
+
+Add following methods to the class:
+- `veto` — returns `No, I must disagree`
+
+- `approve` — returns `You can do that!`
+
+- `doCharity` — returns `I like to help people.`
+
+- `releasePressStatement` — returns `You will see great things from Scuber.`
+
+- `sayHi` — returns `"Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.`
